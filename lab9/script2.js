e70bc050a767b7a18eb00e85477f69f105dceae7
ColeBartley <143630732+ColeBartley@users.noreply.github.com>
2023-11-01 10:52:16 -0500

    Files for EC lab2

---
+++ lab9/script2.js
@@ -0,0 +1,14 @@
+//function that changes the color of border of paragraph when user presses button
+function change_p_color(){
+    //document.getElementById("test").innerHTML = document.getElementById("bR").value;
+    let borderR = document.getElementById("bR").value;
+    let borderG = document.getElementById("bG").value;
+    let borderB = document.getElementById("bB").value;
+    let borderwidth = document.getElementById("width").value;
+    let backgroundR = document.getElementById("backR").value;
+    let backgroundG = document.getElementById("backG").value;
+    let backgroundB = document.getElementById("backB").value;
+
+    document.getElementById("test").style.border = borderwidth + 'px solid '+ 'rgb(' + borderR + ',' + borderG + ',' + borderB + ')';
+    document.getElementById("myDiv").style.backgroundColor = 'rgb(' + backgroundR + ',' + backgroundG + ',' + backgroundB + ')';
+}
